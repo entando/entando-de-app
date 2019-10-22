229fdd3572bcf8bab431ae67c9f87423755c027b
Ampie Barnard <a.barnard@entando.com>
2019-10-22 18:02:17 +0200

    Fixed the keycloak integration

--- build-image.sh
+++ build-image.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-HOST=$(kubectl config view --minify -o go-template='{{with index .clusters 0 }}{{.cluster.server}}{{end}}'|awk -F[/:] '{print $4}')
+#HOST=$(kubectl config view --minify -o go-template='{{with index .clusters 0 }}{{.cluster.server}}{{end}}'|awk -F[/:] '{print $4}')
+HOST=docker-registry-default.apps.serv.run
 USER=$(kubectl config view --minify -o go-template='{{with index .users 0 }}{{.user.username}}{{end}}')
 PWD=$(kubectl config view --minify -o go-template='{{with index .users 0 }}{{.user.password}}{{end}}')
-docker login -u $USER  -p $PWD $HOST:32000
-mvn clean package -DskipTests -Pwildfly -Pmysql
-docker tag entando/entando-de-app-mysql:5.2.0-SNAPSHOT $HOST:32000/entando/entando-de-app-mysql:5.2.0-SNAPSHOT
-docker tag entando/entando-de-app-wildfly:5.2.0-SNAPSHOT $HOST:32000/entando/entando-de-app-wildfly:5.2.0-SNAPSHOT
-docker push $HOST:32000/entando/entando-de-app-mysql:5.2.0-SNAPSHOT
-docker push $HOST:32000/entando/entando-de-app-wildfly:5.2.0-SNAPSHOT
+docker login -u $USER  -p $PWD $HOST
+mvn clean package -DskipTests -Pwildfly -Pderby
+docker tag entando/entando-de-app-wildfly:6.0.0-SNAPSHOT $HOST/entando/entando-de-app-wildfly:6.0.0-SNAPSHOT
+docker push $HOST/entando/entando-de-app-wildfly:6.0.0-SNAPSHOT
