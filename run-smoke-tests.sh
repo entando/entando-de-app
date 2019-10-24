7004521dde82bcc04e7fb9625a659ab6879edb84
Ampie Barnard <a.barnard@entando.com>
2019-10-24 12:15:09 +0200

    Purging all SNAPSHOTS before build

--- run-smoke-tests.sh
+++ run-smoke-tests.sh
@@ -7,11 +7,12 @@ DBMS=${2:-mysql}
 echo "Running smoke tests for ${APPLICATION_NAME} using $JEE_SERVER and $DBMS"
 
 #pre-test cleanup
-mvn -DskipDocker=false docker:stop 2>/dev/null
+mvn -DskipDocker=false -P${JEE_SERVER} -P${DBMS} docker:stop 2>/dev/null
 docker volume rm ${APPLICATION_NAME}-entando-data ${APPLICATION_NAME}-entando-db-data 2>/dev/null
 
 #run full tests
-mvn clean package -P${JEE_SERVER} -P${DBMS} docker:start || { echo "Maven build failed"; exit 1; }
+mvn dependency:purge-local-repository -DsnapshotsOnly=true 
+mvn clean package   -P${JEE_SERVER} -P${DBMS} docker:start || { echo "Maven build failed"; exit 1; }
 docker run --rm --network=${APPLICATION_NAME}-network \
     -e ENTANDO_APPBUILDER_URL=http://appbuilder:5000  \
     entando/entando-smoke-tests:5.1.0-SNAPSHOT mvn verify -Dtest=org.entando.selenium.smoketests.STAddTestUserTest \
