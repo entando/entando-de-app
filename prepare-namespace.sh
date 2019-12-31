43817330e8ada9b0022d605b1228e441abef8022
Ampie Barnard <a.barnard@entando.com>
2019-12-31 17:49:43 +0200

    Running helm without tiller now

--- prepare-namespace.sh
+++ prepare-namespace.sh
@@ -4,5 +4,6 @@ if ! kubectl get namespace ${NAMESPACE}; then
   kubectl create -f charts/role.yaml -n ${NAMESPACE}
   kubectl create rolebinding default-can-access-entando-resources --role=entando-resource-access \
     --serviceaccount=${NAMESPACE}:default -n ${NAMESPACE}
-  helm install entando-operator entando-k8s-controller-coordinator --repo http://jenkins-x-chartmuseum-jx.apps.serv.run/ --namespace ${NAMESPACE}  --wait
+  helm install entando-operator entando-k8s-controller-coordinator \
+     --repo http://jenkins-x-chartmuseum-jx.apps.serv.run/ --namespace ${NAMESPACE} | kubectl apply -f -
 fi
