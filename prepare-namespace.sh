3b7b2b30380aa3dac868acdb57d235b0a2793beb
Ampie Barnard <a.barnard@entando.com>
2019-12-31 17:36:29 +0200

    Preparing namespace from bash now

--- prepare-namespace.sh
+++ prepare-namespace.sh
@@ -1,8 +1,8 @@
 NAMESPACE=$1
 if ! kubectl get namespace ${NAMESPACE}; then
-  kubectl create namespace $NAMESPACE}
-  kubectl create -f charts/
-fi
-if ! kubectl get deployment entando-operator-entando-k8s-controller-coordinator -n ${NAMESPACE}; then
+  kubectl create namespace ${NAMESPACE}
+  kubectl create -f charts/role.yaml -n ${NAMESPACE}
+  kubectl create rolebinding default-can-access-entando-resources --role=entando-resource-access \
+    --serviceaccount=${NAMESPACE}:default -n ${NAMESPACE}
   helm install entando-operator entando-k8s-controller-coordinator --repo http://jenkins-x-chartmuseum-jx.apps.serv.run/ --namespace ${NAMESPACE}  --wait
 fi
