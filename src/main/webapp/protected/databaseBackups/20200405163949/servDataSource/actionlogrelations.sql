b5ff03c39096a8547d21d78c22c2c55b684f4564
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2020-07-30 16:42:19 +0200

    ENG-964: Removed backup

--- src/main/webapp/protected/databaseBackups/20200405163949/servDataSource/actionlogrelations.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO actionlogrelations (recordid,refgroup) VALUES (1,'free');
-INSERT INTO actionlogrelations (recordid,refgroup) VALUES (2,'free');
-INSERT INTO actionlogrelations (recordid,refgroup) VALUES (3,'free');
